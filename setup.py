91675a48150b11b0b9930102580d186439280e1d
Jenny Yang <jennyyang@Jennys-MacBook-Pro-2.local>
2022-12-27 08:57:08 -0800

    setuptools added

---
+++ setup.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="BiasMitigationRL", 
+    version="1",
+    author="Jenny Yang",
+    author_email="jenny.yang@eng.ox.ac.uk",
+    description="BiasMitigationRL provides automatic training of classifier, while mitigating chosen bias",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/yangjenny/reinforcement_learning_bias_mitigation/",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        'tensorflow',
+        'numpy',
+        'pandas',
+        'pickle',
+        'random',
+        'os',
+        'sklearn',
+        'shap',
+        'math',
+        'collections',
+        'argparse',
+        'ast'
+    ]
+)
