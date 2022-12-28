7e09e3cee6a00ffb5a3416bd81c3b77f7217ff36
yangjenny <yangjy259@gmail.com>
2022-12-28 13:40:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        "Operating System :: OS Independent",
+        'License :: MIT License',
+        'Operating System :: OS Independent'
     ],
     package_dir={"": "."},
     packages=setuptools.find_packages(),
